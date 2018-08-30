698909a9f1ad6a7081f2d9f5ece9d03bc3aff4d4
root <root@ip-172-31-44-83.ec2.internal>
2018-08-30 05:48:08 +0000

    added credentials

--- join-and-count/main.tf
+++ join-and-count/main.tf
@@ -1,4 +1,9 @@
 #----------storage/main.tf-------
+provider "aws" {
+  region = "ap-south-1"
+  access_key= "AAI4ABJCTLA3H4EAXA"
+  secret_key= "hJadH+tTn5F8WEyXizwwwr2H81ZgqQ36qPQNA8"
+}
 
 # Create a random id 
 
